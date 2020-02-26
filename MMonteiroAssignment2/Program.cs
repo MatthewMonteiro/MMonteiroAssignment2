6fe43a5951bc87cea2d954bbd9ab7b74a1e927ce
Matthew Monteiro <Monteiro_4@outlook.com>
2020-02-26 12:07:09 -0500

    first commit, TriangleSolver class complete

---
+++ MMonteiroAssignment2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMonteiroAssignment2
+{
+    class Program
+    {
+        /// <summary>
+        /// program that lets the user enter three integers and determines if they make a triangle and what type of 
+        /// triangle they make
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.WriteLine("Welcome To Triangle Solver!");
+                Console.WriteLine();
+                Console.WriteLine("1. Enter Triangle Dimensions:");
+                Console.WriteLine("2. Exit:");
+                string userChoice = Console.ReadLine();
+
+
+                Console.WriteLine("Please Enter Three Sides of a Triangle");
+            }
+        }
+    }
+}
---
+++ MMonteiroAssignment2/TriangleSolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMonteiroAssignment2
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int side1, int side2, int side3)
+        {
+            string response = "Input values do not make a triangle";
+            //check if three sides make triangle at all
+            if(side1 + side2 > side3 || side1 + side3 > side2 || side2 + side3 > side1)
+            {
+                if(side1 == side2 && side2 == side3 && side1 == side3)
+                {
+                    response = "This is a valid equalateral triangle";
+                }
+                else if(side1 != side2 && side2 != side3 && side1 != side3)
+                {
+                    response = "This is a valid scalene triangle";
+                }
+                else
+                {
+                    response = "This is a valid isoceles triangle";
+                }
+            }
+
+            return response;
+        }
+    }
+}
